e815e0235e0f8d152e680fcd9419bf85717a2eca
kishlay1210 <mr.kishlay@gmail.com>
2024-10-03 23:39:57 +0100

    Implementing 24 hrs forecast

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,11 @@ import { weatherCodes } from "./constants";
 
 const App = () => {
   const [currentWeather, setCurrentWeather] = useState({});
+  const [hourlyForecasts, setHourlyForecasts] = useState([]);
+
+  const filterHourlyForecast = (hourlyData) => {
+  setHourlyForecasts(hourlyData);
+};
 
   // Fetches weather details based on API
   const getWeatherDetails = async(API_URL) => {
@@ -16,9 +21,15 @@ const App = () => {
       //Extract Current weather data
       const temperature = Math.floor(data.current.temp_c);
       const description = data.current.condition.text;
-      const weatherIcon = Object.keys(weatherCodes).find(icon => weatherCodes[icon].includes(data.current.condition.code));
+      const weatherIcon = Object.keys(weatherCodes).find((icon) => weatherCodes[icon].includes(data.current.condition.code));
       
       setCurrentWeather({ temperature, description, weatherIcon })
+      
+      // Combine hourly data from forecast days
+      const combinedHourlyData = [...data.forecast.forecastday[0].hour];
+      filterHourlyForecast(combinedHourlyData);
+      // console.log(combinedHourlyData);
+      
     }catch(error){
       console.log(error);
     }
@@ -35,7 +46,10 @@ const App = () => {
         {/*Hourly Weather forecast list*/}
         <div className="hourly-forecast">
           <ul className="weather-list">
-           <HourlyWeatherItem/>
+            {hourlyForecasts.map((hourlyWeather) => (
+              <HourlyWeatherItem key={hourlyWeather.time_epoch} hourlyWeather={hourlyWeather}/>
+            ))}
+           
           </ul>
         </div>
       </div>
